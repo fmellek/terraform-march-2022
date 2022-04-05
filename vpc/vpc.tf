5b9489f3fe403e260864ad789341409a7183b06f
fmellek <ellek528@gmail.com>
2022-04-05 18:08:24 -0500

    syntax has been fixed under route.

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -65,7 +65,7 @@ resource "aws_route_table_association" "third" {
 resource "aws_route" "ingress_route" {
     route_table_id = aws_route_table.public_route_table.id
     destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet-gw
+    gateway_id = aws_internet_gateway.internet-gw.id
 
   
 }
