976b9a206f6feb0f45f94b42e589c6ddb97ba7e2
fmellek <ellek528@gmail.com>
2022-04-05 18:07:17 -0500

    in the public route table route has been edited

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -61,3 +61,11 @@ resource "aws_route_table_association" "third" {
     subnet_id = aws_subnet.public_3.id
     route_table_id = aws_route_table.public_route_table.id
 }
+
+resource "aws_route" "ingress_route" {
+    route_table_id = aws_route_table.public_route_table.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-gw
+
+  
+}
