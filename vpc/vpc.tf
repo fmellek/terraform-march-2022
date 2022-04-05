e5bbf003269702064db9db20e37023fb390ea757
fmellek <ellek528@gmail.com>
2022-04-05 16:22:46 -0500

    syntax on internet gateway resource has been fixed.

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -9,6 +9,6 @@ resource "aws_vpc" "my_custom_vpc" {
 resource "aws_internet_gateway" "internet-gw" {
     vpc_id = aws_vpc.my_custom_vpc.id
     tags = {
-        Name = var.igw_tag 
+        Name = var.igw-tag 
     }
 }
