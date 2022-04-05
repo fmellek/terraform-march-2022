35f6b4e8db8ce556ffc8ef21a63cabb708819906
fmellek <ellek528@gmail.com>
2022-04-04 21:10:27 -0500

    session-4 ec2.tf has been modified.

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -2,11 +2,13 @@ resource "aws_instance" "my_ec2" {        # if you're using "_" always use "_"
   ami           =  data.aws_ami.amazon_linux_2.image_id # String, Hard Coded Value # var = variable, variable name
   instance_type = var.instance_type
   tags = {
-    Name = "${var.env}-app-instance" 
-    Name2 = format("%s-app-instance", var.env)
+    Name = "${var.env}-app-instance"             #s syntax may change. ${variable}
+    Name2 = format("%s-app-instance", var.env)   # syntax will never change
   }
   vpc_security_group_ids = [aws_security_group.my_sg.id]
 } 
 
 # Combine static and dynamic values together. 
 # Name = dev-app-instance, qa-app-instance, prod-app-instance
+# ${} was supported with every single reference befor Terraform Version 0.12
+# They got rid of ${} after 0.12
