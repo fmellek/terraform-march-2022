b71f73ef125817669bb12189ae20d537ff276763
fmellek <ellek528@gmail.com>
2022-04-06 16:45:24 -0500

    security group inbound and outbound rule has been configured.

--- vpc/sg.tf
+++ vpc/sg.tf
@@ -3,3 +3,26 @@ resource "aws_security_group" "my_sg" {
     description = "This is a test security group."
     vpc_id = aws_vpc.my_custom_vpc.id
   }
+
+resource "aws_security_group_rule" "ingress" {
+    count = length(var.sg_ingress_rules)
+
+    type = var.tag_security_group_rule[0]
+    from_port = var.sg_ingress_rules[count.index].from_port
+    to_port = var.sg_ingress_rules[count.index].to_port
+    protocol = var.sg_ingress_rules[count.index].protocol
+    cidr_blocks = [var.sg_ingress_rules[count.index].cidr_blocks]
+    description = var.sg_ingress_rules[count.index].description
+    security_group_id = aws_security_group.my_sg.id
+
+  }
+
+resource "aws_security_group_rule" "egress" {
+  type = var.tag_security_group_rule[1]
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1" # This is known by aws it means all ports are open either protocol = -1 or protocol = all same thing.
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.my_sg.id 
+  
+}
--- vpc/variables_sg.tf
+++ vpc/variables_sg.tf
@@ -3,3 +3,37 @@ variable "tag_sg" {
     description = "This variable represent the name of the security group"
     default = "WordPress-Web-Security-Group"
   }
+
+variable "sg_ingress_rules" {
+    type = list(object({
+        from_port = number
+        to_port = number
+        protocol = string
+        cidr_blocks = string
+        description = string
+    }))
+    default = [ {
+      cidr_blocks = "0.0.0.0/0"
+      description = "test"
+      from_port = 22
+      protocol = "tcp"
+      to_port = 22
+    },
+    { 
+      cidr_blocks = "0.0.0.0/0"
+      description = "test"
+      from_port = 80
+      protocol = "tcp"
+      to_port = 80
+
+    },
+    ]
+  
+}
+
+variable "tag_security_group_rule" {
+    type = list
+    description = "This variable represent the name of the security group rule."
+    default = ["ingress", "egress"]
+  
+}
