a7ff0402e3b82d25792c698e3d90ae1a22fd3603
fmellek <ellek528@gmail.com>
2022-04-06 16:18:37 -0500

    security group has been attached to WordPress-Web instance.

--- vpc/ec2.tf
+++ vpc/ec2.tf
@@ -7,5 +7,6 @@ resource "aws_instance" "my_ec2" {
     subnet_id = aws_subnet.public_1.id
     key_name = var.key_name
     associate_public_ip_address = true
+    vpc_security_group_ids = [aws_security_group.my_sg.id]
 
 }
