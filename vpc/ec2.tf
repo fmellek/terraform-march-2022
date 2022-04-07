88b6ea5ba1f1e050c1ae84e39ee42b96b1a6d31f
fmellek <ellek528@gmail.com>
2022-04-07 16:22:58 -0500

    tagging in ec2 has been cleaned up also from variables_ec2.tf

--- vpc/ec2.tf
+++ vpc/ec2.tf
@@ -2,10 +2,10 @@ resource "aws_instance" "my_ec2" {
     ami = data.aws_ami.amazon_linux_2.image_id
     instance_type = var.instance_type
     tags = {
-      Name = var.instance_name
+      Name = "WordPress-Web"
     }
     subnet_id = aws_subnet.public_1.id
-    key_name = var.key_name
+    key_name = "Terraform-Server-Key"
     associate_public_ip_address = true
     vpc_security_group_ids = [aws_security_group.my_sg.id]
     user_data = file("web-userdata.sh")
--- vpc/variables_ec2.tf
+++ vpc/variables_ec2.tf
@@ -4,14 +4,3 @@ variable "instance_type" {
     default = "t2.micro"
 }
 
-variable "instance_name" {
-    type = string
-    description = "This variable represent the name of the EC2 instance."
-    default = "Wordpress-Web"
-  }
-
-variable "key_name" {
-    type = string
-    description = "This variable represent the name of the key-pair"
-    default = "Terraform-Server-Key"
-  }
