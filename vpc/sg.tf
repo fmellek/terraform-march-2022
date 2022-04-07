ba517baa4f4d33b018d4af72bd58f83a03fe4989
fmellek <ellek528@gmail.com>
2022-04-07 15:22:38 -0500

    code has been modified for security group and security

--- vpc/sg.tf
+++ vpc/sg.tf
@@ -11,7 +11,7 @@ resource "aws_security_group_rule" "ingress" {
     from_port = element(var.sg_ingress_rules, count.index)
     to_port = element(var.sg_ingress_rules, count.index)
     protocol = "tcp"
-    cidr_blocks = element(var.sg_inbound_rules_cidr, count.index)
+    cidr_blocks = var.sg_inbound_rules_cidr
     description = "This is the inbound rules."
     security_group_id = aws_security_group.my_sg.id
 
