e86df1040b5b961d2fa54ef7b76648acd1b625a5
fmellek <ellek528@gmail.com>
2022-04-07 16:39:52 -0500

    WordPress-Database ec2_instance has been created.

--- vpc/ec2.tf
+++ vpc/ec2.tf
@@ -13,3 +13,19 @@ resource "aws_instance" "my_ec2" {
     user_data = file("web-userdata.sh")
 
 }
+
+resource "aws_instance" "my_ec2" {
+    ami = data.aws_ami.amazon_linux_2.image_id
+    instance_type = var.instance_type
+    tags = {
+      Name = "WordPress-Database"
+      ENV = "dev"
+      Project = "VPC"
+    }
+    subnet_id = aws_subnet.private_1.id
+    key_name = "Terraform-Server-Key"
+    associate_public_ip_address = true
+    vpc_security_group_ids = [aws_security_group.my_sg.id]
+   
+
+}
