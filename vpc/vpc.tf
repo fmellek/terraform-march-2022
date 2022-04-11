d2d11660189af07e5fc0798d9f21548ef9261efa
fmellek <ellek528@gmail.com>
2022-04-11 16:57:59 -0500

    provider has been added to resources.

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -29,6 +29,7 @@ resource "aws_vpc" "my_custom_vpc_california" {
 
 resource "aws_internet_gateway" "internet-gw" {
     vpc_id = aws_vpc.my_custom_vpc.id
+    
     tags = merge( 
       var.tags, 
       {
@@ -40,6 +41,7 @@ resource "aws_internet_gateway" "internet-gw" {
 
 resource "aws_internet_gateway" "internet-gw_california" {
     vpc_id = aws_vpc.my_custom_vpc_california.id
+    provider = aws.us-west-1
     provider = aws.us-west-1
     tags = merge( 
       var.tags, 
@@ -63,6 +65,22 @@ resource "aws_subnet" "public_subnet" {
     )
 }
 
+resource "aws_subnet" "public_subnet_california" {
+  count = length(var.az_name)
+
+    vpc_id = aws_vpc.my_custom_vpc_california.id
+    provider = aws.us-west-1
+    availability_zone = element(var.az_name, count.index)
+    cidr_block = element(var.public_cidr_subnet, count.index)
+    tags = merge(
+      var.tags,
+      {
+      Name = "public_subnet_california_${count.index}"
+    }
+    )
+}
+
+
 resource "aws_subnet" "private_subnet" {
   count = length(var.az_name)
     vpc_id = aws_vpc.my_custom_vpc.id
@@ -72,6 +90,20 @@ resource "aws_subnet" "private_subnet" {
       var.tags,
       {
       Name = "private_subnet_${count.index}"
+    }
+    )
+}
+
+resource "aws_subnet" "private_subnet_california" {
+  count = length(var.az_name)
+    vpc_id = aws_vpc.my_custom_vpc_california.id
+    provider = aws.us-west-1
+    availability_zone = element(var.az_name, count.index)
+    cidr_block = element(var.private_cidr_subnet, count.index)
+    tags = merge(
+      var.tags,
+      {
+      Name = "private_subnet_california_${count.index}"
     }
     )
 }
@@ -90,6 +122,16 @@ resource "aws_route_table" "public_route_table" {
     )
 }
 
+resource "aws_route_table" "public_route_table_california" {
+    vpc_id = aws_vpc.my_custom_vpc_california.id
+    provider = aws.us-west-1
+    tags = merge(
+      var.tags,{ 
+      Name = "public_route_table_california"
+    }
+    )
+}
+
 resource "aws_route_table_association" "public" {
   count = length(var.az_name)
   subnet_id = element(aws_subnet.public_subnet.*.id, count.index)
@@ -97,10 +139,26 @@ resource "aws_route_table_association" "public" {
   
 }
 
+resource "aws_route_table_association" "public_california" {
+  count = length(var.az_name)
+  provider = aws.us-west-1
+  subnet_id = element(aws_subnet.public_subnet_california.*.id, count.index)
+  route_table_id = element(aws_route_table.public_route_table_california.*.id, count.index)
+  
+}
+
 resource "aws_route" "ingress_route" {
     route_table_id = aws_route_table.public_route_table.id
     destination_cidr_block = var.destination_cidr_public
     gateway_id = aws_internet_gateway.internet-gw.id
+
+  }
+
+  resource "aws_route" "ingress_route_california" {
+    route_table_id = aws_route_table.public_route_table_california.id
+    provider = aws.us-west-1
+    destination_cidr_block = var.destination_cidr_public
+    gateway_id = aws_internet_gateway.internet-gw_california.id
 
   }
 
@@ -113,6 +171,15 @@ resource "aws_route" "ingress_route" {
     )
 }
 
+ resource "aws_route_table" "private_route_table_california" {
+    vpc_id = aws_vpc.my_custom_vpc_california.id
+    provider = aws.us-west-1
+    tags = merge(
+      var.tags,{
+      Name = "private_route_table_california"
+    }
+    )
+}
 
 
 
@@ -126,9 +193,21 @@ resource "aws_route_table_association" "private" {
   route_table_id = element(aws_route_table.private_route_table.*.id, count.index)
 }
 
+resource "aws_route_table_association" "private_california" {
+  count = length(var.az_name)
+  provider = aws.us-west-1
+  subnet_id = element(aws_subnet.private_subnet_california.*.id, count.index)
+  route_table_id = element(aws_route_table.private_route_table_california.*.id, count.index)
+}
+
  resource "aws_eip" "nat" {
      depends_on = [aws_internet_gateway.internet-gw]
  } 
+
+ resource "aws_eip" "nat_california" {
+   provider = aws.us-west-1
+     depends_on = [aws_internet_gateway.internet-gw_california]
+ }
 
  resource "aws_nat_gateway" "nat_gateway" {
    allocation_id = aws_eip.nat.id
@@ -139,6 +218,15 @@ resource "aws_route_table_association" "private" {
      Project = "VPC"
    }
    
+    resource "aws_nat_gateway" "nat_gateway_california" {
+      provider = aws.us-west-1
+   allocation_id = aws_eip.nat_california.id
+   subnet_id = aws_subnet.public_subnet_california[0].id
+   tags = {
+     Name = "nat_gateway_california"
+     ENV = "dev"
+     Project = "VPC"
+   }
  }
 
  resource "aws_route" "ingress_route_nat_gateway" {
@@ -146,5 +234,14 @@ resource "aws_route_table_association" "private" {
     destination_cidr_block = var.destination_cidr_public
     
     nat_gateway_id = aws_nat_gateway.nat_gateway.id
+
+  }
+
+   resource "aws_route" "ingress_route_nat_gateway_california" {
+     provider = aws.us-west-1
+    route_table_id = aws_route_table.private_route_table_california.id
+    destination_cidr_block = var.destination_cidr_public
+    
+    nat_gateway_id = aws_nat_gateway.nat_gateway_california.id
 
   }
