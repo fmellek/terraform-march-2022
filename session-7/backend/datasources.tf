2c71922ffa6cce29bf7656784e93da4d536aed7c
fmellek <ellek528@gmail.com>
2022-04-11 20:43:06 -0500

    data source is added.

---
+++ session-7/backend/datasources.tf
@@ -0,0 +1,17 @@
+# This feature is called data sourcing the remote backend
+# Data source only fetches the from outputs
+
+data "terraform_remote_state" "backend" {
+    backend = "s3"
+    config = {
+         bucket = "terraform-march-2022-backend-bucket-fme"
+        key = "session-7/backend/terraform.tfstate"
+        region = "us-west-1"
+    }
+}
+
+
+output "password" {
+    value = data.terraform_remote_state.backend.outputs.db_password
+  
+} # output from the output
