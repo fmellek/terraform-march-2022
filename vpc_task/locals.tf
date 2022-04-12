fe1bfd7d2f53f2f9ee4cba87862791d25b01419b
fmellek <ellek528@gmail.com>
2022-04-12 17:46:12 -0500

    syntax error has been fixed on vpc.tf

--- vpc_task/locals.tf
+++ vpc_task/locals.tf
@@ -1,5 +1,4 @@
 locals {
-  name = "aws-${var.stage}-${var.region}-${var.env}-${var.project}-rtype"
   common_tags = {
       env = var.env
       PROJECT = var.project
--- vpc_task/vpc.tf
+++ vpc_task/vpc.tf
@@ -1,11 +1,13 @@
 resource "aws_vpc" "my_vpc_east" {
     cidr_block = var.vpc_cidr_block
     instance_tenancy = var.instance_tenancy
-    tags = {
-        Name = replace(local.name, "rytpe", "my_vpc_east"),
-        Name2 = local.common_tags
+    tags = merge(
+        local.common_tags, {
+            Name = "my_vpc"
+       
+        
     }
-    
+    )
 }
 
 
