95167cb91ba4bcd2822e736b89283c82e087ca51
fmellek <ellek528@gmail.com>
2022-04-12 17:48:19 -0500

    syntax fixed on vpc.tf

--- vpc_task/vpc.tf
+++ vpc_task/vpc.tf
@@ -2,11 +2,12 @@ resource "aws_vpc" "my_vpc_east" {
     cidr_block = var.vpc_cidr_block
     instance_tenancy = var.instance_tenancy
     tags = merge(
-        local.common_tags, {
+         {
             Name = "my_vpc"
        
         
-    }
+    },
+    local.common_tags
     )
 }
 
