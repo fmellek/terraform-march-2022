e6df6381c66cb18536dd76760872451fb2d7dd05
fmellek <ellek528@gmail.com>
2022-04-12 15:44:41 -0500

    basic steps of the vpc task.

--- vpc_task/backend.tf
+++ vpc_task/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "terraform-march-2022-backend-bucket-fme"
-        key = "vpc_task/terraform.tfstate"
+        key = "vpc_task/us-east-1/terraform.tfstate"
         region = "us-west-1"
         dynamodb_table= "terraform-march-2022-lock"
     }
---
+++ vpc_task/vpc.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "my_vpc_east" {
+    cidr_block = var.vpc_cidr_block
+    instance_tenancy = var.instance_tenancy
+    tags = {
+        Name = "My_VPC"
+    }
+}
+
+
+resource "aws_vpc" "my_vpc_west" {
+    cidr_block = var.vpc_cidr_block
+    instance_tenancy = var.instance_tenancy
+    tags = {
+        Name = "My_VPC"
+    }
+    provider = aws.us
+}
