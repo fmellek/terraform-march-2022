085e1a8353f6e0421b4fd667acbf09849dfbe023
fmellek <ellek528@gmail.com>
2022-04-12 17:43:11 -0500

    syntax error has been fixed on vpc.tf

--- vpc_task/vpc.tf
+++ vpc_task/vpc.tf
@@ -2,10 +2,10 @@ resource "aws_vpc" "my_vpc_east" {
     cidr_block = var.vpc_cidr_block
     instance_tenancy = var.instance_tenancy
     tags = {
-        Name = replace(local.name, "rytpe" "my_vpc_east"),
+        Name = replace(local.name, "rytpe", "my_vpc_east"),
         Name2 = local.common_tags
     }
-    )
+    
 }
 
 
