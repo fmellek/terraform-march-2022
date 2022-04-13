645b2fe895d5e1269061cd17a13634767d76ff74
fmellek <ellek528@gmail.com>
2022-04-13 13:47:28 -0500

    route table associations has been completed.

--- vpc_task/vpc.tf
+++ vpc_task/vpc.tf
@@ -101,3 +101,19 @@ resource "aws_route_table" "private_rtb" {
     )
   
 }
+
+resource "aws_route_table_association" "public" {
+    count = length(var.subnet_name)
+
+    subnet_id = element(aws_subnet.public_subnet.*.id, count.index)
+    route_table_id = element(aws_route_table.public_rtb.*.id, count.index)
+  
+}
+
+resource "aws_route_table_association" "private" {
+    count = length(var.subnet_name)
+
+    subnet_id = element(aws_subnet.private_subnet.*.id, count.index)
+    route_table_id = element(aws_route_table.private_rtb.*.id, count.index)
+  
+}
