7625dd6f1e6a75641bdac27ba47f2a950ad1c93f
fmellek <ellek528@gmail.com>
2022-04-13 20:32:22 -0500

    File provisioner, Key Pair added.

--- session-8/ec2.tf
+++ session-8/ec2.tf
@@ -9,5 +9,23 @@ resource "aws_instance" "my_ec2" {        # if you're using "_" always use "_"
      }
      )
   vpc_security_group_ids = [aws_security_group.my_sg.id]
-  user_data = file("user_data.sh")
+  key_name = aws_key_pair.terraform_server.key_name
+
+  provisioner "file" {
+    source = "/home/ec2-user/terraform-march-2022/session-8/index.html" # Where does your file exist? Your Terraform Server
+    destination = "/tmp/index.html" # This is the remote server
+    
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      host = self.public_ip
+      private_key = file("~/.ssh/id_rsa")       # Private key of my Terraform Server
+    }
+  
+  }
+ 
 } 
+resource "aws_key_pair" "terraform_server" {
+  key_name = replace(local.name, "rtype", "terraform_server_key")
+  public_key = file("~/.ssh/id_rsa.pub")
+}
