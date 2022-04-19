a27ebc466a30d3534003edbc97dac8170c28f026
fmellek <ellek528@gmail.com>
2022-04-18 20:34:17 -0500

    dynamic block is added to sg.tf

---
+++ modules/sg/sg.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "main" {
+    name = format("%s-instance-sg", var.env)
+    description = "This is a security group for my ec2"
+
+    # Dynamic Block was implemented Terraform Version 0.12 
+    dynamic "ingress" {   # Instead of using element function you can go head and use dynamic block.
+        for_each = var.ports
+        content = {
+            from_port = ingress.value.from_port
+            to_port =  ingress.value.to_port
+            protocol = ingress.value.cidr_blocks
+            cidr_blocks = ingress.value.protocol
+        }
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  
+}
